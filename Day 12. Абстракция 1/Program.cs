c8d568af6f6860fd544deb10dd43d6bddd54d5f2
Atamoghlan <atamoghlan.a.aliyev@gmail.com>
2020-09-17 13:24:18 +0400

    Abs

---
+++ Day 12. Абстракция 1/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace Day_11_Задание_1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Student Atamoghlan = new Student();
+            Aspirant Araz = new Aspirant();
+            Console.WriteLine("Ввести студента - 1\nВвести аспиранта - 2\nВыйти - 3");
+            string answer = Console.ReadLine();
+            switch (answer)
+            {
+                case ("1"):
+                    while (answer == "1")
+                    {
+                        string lastname = "";
+                        int course = 0;
+                        int creditbooknumber = 0;
+                        while (lastname == null || lastname.Length < 2 || lastname.Length > 30)
+                        {
+                            Console.WriteLine("Введите фамилию студента");
+                            lastname = Console.ReadLine();
+                            if (lastname != null && lastname.Length > 2 && lastname.Length < 30)
+                            {
+                                Atamoghlan.Lastname = lastname;
+                                continue;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ошибка! Фамилия должна состоять из английских букв и быть не длиннее 30-ти символов");
+                            }
+                        }
+                        while (course < 1 || course > 5)
+                        {
+                            Console.WriteLine("Введите курс студента");
+                            course = Convert.ToInt32(Console.ReadLine());
+                            if (course > 0 && course < 6)
+                            {
+                                Atamoghlan.Course = course;
+                                continue;
+                            }
+                            else { Console.WriteLine("Ошибка!"); }
+                        }
+                        while (creditbooknumber < 1 || creditbooknumber > 10000)
+                        {
+                            Console.WriteLine("Введите номер зачетной книги");
+                            creditbooknumber = Convert.ToInt32(Console.ReadLine());
+                            if (creditbooknumber > 0 && creditbooknumber < 10000)
+                            {
+                                Atamoghlan.CreditBookNumber = creditbooknumber;
+                                continue;
+                            }
+                            else { Console.WriteLine("Ошибка!"); }
+                        }
+                        Atamoghlan.Print();
+
+
+                        Console.WriteLine("Ввести студента - 1\nВвести аспиранта -2\nВыйти -3");
+                        answer = Console.ReadLine();
+
+
+                    }
+                    break;
+
+                case ("2"):
+                    while (answer == "2")
+                    {
+                        string lastname2;
+                        int course2 = 0;
+                        int creditbooknumber2 = 0;
+                        int dissertation = 0;
+                        while (Araz.Lastname == null || Araz.Lastname.Length < 2 || Araz.Lastname.Length > 30)
+                        {
+                            Console.WriteLine("Введите фамилию аспиранта");
+                            lastname2 = Console.ReadLine();
+                            if (lastname2 != null && lastname2.Length > 2 && lastname2.Length < 30)
+                            { Araz.Lastname = lastname2; continue; }
+                            else
+                            {
+                                Console.WriteLine("Ошибка! Фамилия должна состоять из английских букв и быть не длиннее 30-ти символов");
+                            }
+                        }
+                        while (course2 < 1 || course2 > 5)
+                        {
+                            Console.WriteLine("Введите курс аспиранта");
+                            course2 = Convert.ToInt32(Console.ReadLine());
+                            if (course2 > 0 && course2 < 6)
+                            { Araz.Course = course2; continue; }
+                            else { Console.WriteLine("Ошибка!"); }
+                        }
+                        while (creditbooknumber2 < 1 || creditbooknumber2 > 10000)
+                        {
+                            Console.WriteLine("Введите номер зачетной книги");
+                            creditbooknumber2 = Convert.ToInt32(Console.ReadLine());
+                            if (creditbooknumber2 > 0 && creditbooknumber2 < 10000)
+                            { Araz.CreditBookNumber = creditbooknumber2; continue; }
+                            else { Console.WriteLine("Ошибка!"); }
+                        }
+                        while (dissertation < 1 || dissertation > 10000)
+                        {
+                            Console.WriteLine("Введите номер диссертации");
+                            dissertation = Convert.ToInt32(Console.ReadLine());
+                            if (dissertation > 0 && dissertation < 10000)
+                            {
+                                Araz.Dissertation = dissertation;
+                                continue;
+                            }
+                            else { Console.WriteLine("Ошибка!"); }
+                        }
+                        Araz.Print();
+
+                        Console.WriteLine("Ввести студента - 1\nВвести аспиранта -2\nВыйти -3");
+                        answer = Console.ReadLine();
+                        break;
+                    }
+                    break;
+                case ("3"):
+                    break;
+                default:
+                    Console.WriteLine("\t   Ошибка!\nВвести студента - 1\nВвести аспиранта - 2\nВыйти - 3");
+                    answer = Console.ReadLine();
+                    break;
+
+            }
+            Console.WriteLine("System.Object Методы:");
+            Console.WriteLine(Atamoghlan.Course);
+            Console.WriteLine(Atamoghlan.GetType());
+            Console.WriteLine(Atamoghlan.GetHashCode());
+            bool EqualOrNot = Atamoghlan.Course.Equals(Atamoghlan.CreditBookNumber);
+            Console.WriteLine(EqualOrNot);
+
+        }
+    }
+    public abstract class Educated
+    {
+        public abstract string Lastname { get; set; }
+        public override string ToString()
+        {
+            if (String.IsNullOrEmpty(Lastname))
+                return base.ToString();
+            return Lastname;
+        }
+        public abstract int Course { get; set; }
+        public abstract int CreditBookNumber { get; set; }
+        public abstract void Print();
+
+    }
+    public class Student : Educated
+    {
+        public override string Lastname { get; set; }
+
+        public override int Course { get; set; }
+        public override int CreditBookNumber { get; set; }
+        public Student() { }
+       
+        public override void Print()
+        { Console.WriteLine($"Студент {Lastname}учится на {Course}-ом(ем) курсе. Номер зачетной книги: {CreditBookNumber}"); }
+
+    }
+    public class Aspirant : Educated
+    {
+        public override string Lastname { get; set; }
+
+        public override int Course { get; set; }
+        public override int CreditBookNumber { get; set; }
+        public int Dissertation { get; set; }
+        public Aspirant() { }
+        
+        public override void Print()
+        { Console.WriteLine($"Студент {Lastname} учится на {Course}-ом(ем) курсе. Номер зачетной книги: {CreditBookNumber}. Номер диссертации: {Dissertation}"); }
+
+    }
+}
+
---
+++ Day 12. Абстракция 2/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Day_11_Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string a = "";
+            while (a != "Ф" || a != "Ю")
+            {
+                Console.WriteLine("Введите Ф, если вы Физическое лицо\nВведите Ю, если вы Юридическое лицо");
+                a = Console.ReadLine();
+                if (a == "Ф")
+                {
+                    Individual clientF = new Individual();
+                    Console.WriteLine("Введите сумму депозита");
+                    clientF.Sum = Convert.ToInt32(Console.ReadLine());
+                    clientF.NumOfAccount = 1;
+                    clientF.Date = "08.09.2020";
+                    clientF.Percents();
+                    clientF.issueofmoney();
+                    clientF.DateOfAccountOpening();
+                }
+
+                else if (a == "Ю")
+                {
+                    int b = 0;
+                    Company clientY = new Company();
+                    while (b < 1 || b == 0)
+                    {
+                        Console.WriteLine("Введите сумму депозита");
+                        b = Convert.ToInt32(Console.ReadLine());
+                        if (b > 1 && b != 0)
+                        { clientY.Sum = b; }
+                        else { Console.WriteLine("Некорректные данные"); }
+                    }
+
+                    clientY.NumOfAccount = 1;
+                    clientY.Date = "08.09.2020";
+                    clientY.Percents();
+                    clientY.issueofmoney();
+                    clientY.DateOfAccountOpening();
+                }
+
+
+
+                else
+                { Console.WriteLine("Ошибка!"); }
+            }
+        }
+        public class Account
+        {
+
+            public int Sum { get; set;}
+            
+            public int NumOfAccount { get; set; }
+            public string Date { get; set; }
+            public Account() { }
+
+
+            public Account(int sum, int accountnum, string date) { Sum = sum; accountnum = NumOfAccount; Date = date; }
+            public void issueofmoney() { Console.WriteLine($"Сумма:{Sum} Номер аккаунта:{NumOfAccount}"); }
+            public void DateOfAccountOpening() { Console.WriteLine($"Дата открытия счета:{Date}"); }
+
+        }
+        public class Individual : Account
+        {
+
+            public string AccountType { get; set; }
+            public Individual() { }
+            public Individual(int sum, int accountnum, string date, string accounttype) : base(sum, accountnum, date)
+            { AccountType = accounttype; Sum = sum; }
+            public void Percents()
+            {
+                double percent;
+                double FinalSum;
+                if (Sum <= 10000)
+                {
+                    AccountType = "classic";
+                    percent = 1.03;
+                    FinalSum = Sum * percent;
+                    Console.WriteLine($"Предлагаем Вам по тарифу Classic 3% годовых. По окончанию срока вы получите {FinalSum} ");
+                }
+                else if (Sum <= 50000)
+                {
+                    AccountType = "premium";
+                    percent = 1.05;
+                    FinalSum = Sum * percent;
+                    Console.WriteLine($"Предлагаем Вам по тарифу Premium 5% годовых. По окончанию срока вы получите {FinalSum} ");
+                }
+                else if (Sum > 50000)
+                {
+                    AccountType = "gold";
+                    percent = 1.08;
+                    FinalSum = Sum * percent;
+                    Console.WriteLine($"Предлагаем Вам по тарифу Gold 8% годовых. По окончанию срока вы получите {FinalSum} ");
+                }
+            }
+
+            public void CashOut()
+            {
+                Console.WriteLine("Введите сумму вывода");
+                int s = Convert.ToInt32(Console.ReadLine());
+                Sum = Sum - s;
+                Console.WriteLine($"Осталось средств: {Sum}");
+            }
+
+        }
+        public class Company : Account
+
+        {
+            public Company() { }
+            public Company(int sum, int accountnum, string date)
+            : base(sum, accountnum, date)
+            { }
+            public void Percents()
+            {
+                double percent;
+                double FinalSum;
+                if (Sum <= 10000)
+                {
+                    percent = 1.03;
+                    FinalSum = Sum * percent;
+                    Console.WriteLine($"Предлагаем Вам 3% годовых. По окончанию срока вы получите {FinalSum} ");
+                }
+                else if (Sum <= 50000)
+                {
+                    percent = 1.05;
+                    FinalSum = Sum * percent;
+                    Console.WriteLine($"Предлагаем Вам 5% годовых. По окончанию срока вы получите {FinalSum} ");
+                }
+                else if (Sum > 50000)
+                {
+                    percent = 1.08;
+                    FinalSum = Sum * percent;
+                    Console.WriteLine($"Предлагаем Вам 8% годовых. По окончанию срока вы получите {FinalSum} ");
+                }
+            }
+        }
+    }
+}
