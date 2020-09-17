0b78633ea847399dd1c4d20f72f1974b4f31823a
Atamoghlan <atamoghlan.a.aliyev@gmail.com>
2020-09-17 16:17:40 +0400

    final

--- Day 12. Абстракция 1/Program.cs
+++ Day 12. Абстракция 1/Program.cs
@@ -2,12 +2,10 @@
 
 namespace Day_11_Задание_1
 {
-    class Program
+     class Program
     {
         public static void Main()
         {
-            Student Atamoghlan = new Student();
-            Aspirant Araz = new Aspirant();
             Console.WriteLine("Ввести студента - 1\nВвести аспиранта - 2\nВыйти - 3");
             string answer = Console.ReadLine();
             switch (answer)
@@ -15,100 +13,31 @@ namespace Day_11_Задание_1
                 case ("1"):
                     while (answer == "1")
                     {
-                        string lastname = "";
-                        int course = 0;
-                        int creditbooknumber = 0;
-                        while (lastname == null || lastname.Length < 2 || lastname.Length > 30)
-                        {
-                            Console.WriteLine("Введите фамилию студента");
-                            lastname = Console.ReadLine();
-                            if (lastname != null && lastname.Length > 2 && lastname.Length < 30)
-                            {
-                                Atamoghlan.Lastname = lastname;
-                                continue;
-                            }
-                            else
-                            {
-                                Console.WriteLine("Ошибка! Фамилия должна состоять из английских букв и быть не длиннее 30-ти символов");
-                            }
-                        }
-                        while (course < 1 || course > 5)
-                        {
-                            Console.WriteLine("Введите курс студента");
-                            course = Convert.ToInt32(Console.ReadLine());
-                            if (course > 0 && course < 6)
-                            {
-                                Atamoghlan.Course = course;
-                                continue;
-                            }
-                            else { Console.WriteLine("Ошибка!"); }
-                        }
-                        while (creditbooknumber < 1 || creditbooknumber > 10000)
-                        {
-                            Console.WriteLine("Введите номер зачетной книги");
-                            creditbooknumber = Convert.ToInt32(Console.ReadLine());
-                            if (creditbooknumber > 0 && creditbooknumber < 10000)
-                            {
-                                Atamoghlan.CreditBookNumber = creditbooknumber;
-                                continue;
-                            }
-                            else { Console.WriteLine("Ошибка!"); }
-                        }
+                        Student Atamoghlan = new Student(lastname(), course(), creditbooknumber());
                         Atamoghlan.Print();
-
-
+                        Console.WriteLine("\nSystem.Object Методы:");
+                        Console.WriteLine(Atamoghlan.Course);
+                        Console.WriteLine(Atamoghlan.GetType());
+                        Console.WriteLine(Atamoghlan.GetHashCode());
+                        bool EqualOrNot = Atamoghlan.Course.Equals(Atamoghlan.CreditBookNumber);
+                        Console.WriteLine(EqualOrNot);
                         Console.WriteLine("Ввести студента - 1\nВвести аспиранта -2\nВыйти -3");
                         answer = Console.ReadLine();
-
-
                     }
                     break;
 
                 case ("2"):
                     while (answer == "2")
                     {
-                        string lastname2;
-                        int course2 = 0;
-                        int creditbooknumber2 = 0;
-                        int dissertation = 0;
-                        while (Araz.Lastname == null || Araz.Lastname.Length < 2 || Araz.Lastname.Length > 30)
-                        {
-                            Console.WriteLine("Введите фамилию аспиранта");
-                            lastname2 = Console.ReadLine();
-                            if (lastname2 != null && lastname2.Length > 2 && lastname2.Length < 30)
-                            { Araz.Lastname = lastname2; continue; }
-                            else
-                            {
-                                Console.WriteLine("Ошибка! Фамилия должна состоять из английских букв и быть не длиннее 30-ти символов");
-                            }
-                        }
-                        while (course2 < 1 || course2 > 5)
-                        {
-                            Console.WriteLine("Введите курс аспиранта");
-                            course2 = Convert.ToInt32(Console.ReadLine());
-                            if (course2 > 0 && course2 < 6)
-                            { Araz.Course = course2; continue; }
-                            else { Console.WriteLine("Ошибка!"); }
-                        }
-                        while (creditbooknumber2 < 1 || creditbooknumber2 > 10000)
-                        {
-                            Console.WriteLine("Введите номер зачетной книги");
-                            creditbooknumber2 = Convert.ToInt32(Console.ReadLine());
-                            if (creditbooknumber2 > 0 && creditbooknumber2 < 10000)
-                            { Araz.CreditBookNumber = creditbooknumber2; continue; }
-                            else { Console.WriteLine("Ошибка!"); }
-                        }
-                        while (dissertation < 1 || dissertation > 10000)
-                        {
-                            Console.WriteLine("Введите номер диссертации");
-                            dissertation = Convert.ToInt32(Console.ReadLine());
-                            if (dissertation > 0 && dissertation < 10000)
-                            {
-                                Araz.Dissertation = dissertation;
-                                continue;
-                            }
-                            else { Console.WriteLine("Ошибка!"); }
-                        }
+                        
+                        Aspirant Araz = new Aspirant(lastname(), course(), creditbooknumber(), dissertation());
+                        Araz.Print();
+                        Console.WriteLine("\nSystem.Object Методы:");
+                        Console.WriteLine(Araz.Course);
+                        Console.WriteLine(Araz.GetType());
+                        Console.WriteLine(Araz.GetHashCode());
+                        bool EqualOrNot = Araz.Course.Equals(Araz.CreditBookNumber);
+                        Console.WriteLine(EqualOrNot);
                         Araz.Print();
 
                         Console.WriteLine("Ввести студента - 1\nВвести аспиранта -2\nВыйти -3");
@@ -122,26 +51,82 @@ namespace Day_11_Задание_1
                     Console.WriteLine("\t   Ошибка!\nВвести студента - 1\nВвести аспиранта - 2\nВыйти - 3");
                     answer = Console.ReadLine();
                     break;
-
             }
-            Console.WriteLine("System.Object Методы:");
-            Console.WriteLine(Atamoghlan.Course);
-            Console.WriteLine(Atamoghlan.GetType());
-            Console.WriteLine(Atamoghlan.GetHashCode());
-            bool EqualOrNot = Atamoghlan.Course.Equals(Atamoghlan.CreditBookNumber);
-            Console.WriteLine(EqualOrNot);
 
+             string lastname()
+            {
+                string lastname = "";
+                
+                while (lastname == null || lastname.Length < 2 || lastname.Length > 30)
+                {
+                    Console.WriteLine("Введите фамилию студента");
+                    lastname = Console.ReadLine();
+                    if (lastname != null && lastname.Length > 2 && lastname.Length < 30)
+                    {
+
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка! Фамилия должна состоять из английских букв и быть не длиннее 30-ти символов");
+                    }
+                }
+                return lastname;
+                
+            }
+             int course()
+            {
+                int course = 0;
+                while (course < 1 || course > 5)
+                {
+                    Console.WriteLine("Введите курс студента");
+                    course = Convert.ToInt32(Console.ReadLine());
+                    if (course > 0 && course < 6)
+                    {
+
+                        continue;
+                    }
+                    else { Console.WriteLine("Ошибка!"); }
+                }
+                return course;
+            }
+            int creditbooknumber ()
+            {
+                int creditbooknumber = 0;
+                while (creditbooknumber < 1 || creditbooknumber > 10000)
+                {
+                    Console.WriteLine("Введите номер зачетной книги");
+                    creditbooknumber = Convert.ToInt32(Console.ReadLine());
+                    if (creditbooknumber > 0 && creditbooknumber < 10000)
+                    {
+
+                        continue;
+                    }
+                    else { Console.WriteLine("Ошибка!"); }
+                }
+                return creditbooknumber;
+            }
+            int dissertation()
+            {
+                int dissertation = 0;
+                while (dissertation < 1 || dissertation > 10000)
+                {
+                    Console.WriteLine("Введите номер диссертации");
+                    dissertation = Convert.ToInt32(Console.ReadLine());
+                    if (dissertation > 0 && dissertation < 10000)
+                    {
+
+                        continue;
+                    }
+                    else { Console.WriteLine("Ошибка!"); }
+                }
+                return dissertation;
+            }
         }
     }
     public abstract class Educated
     {
         public abstract string Lastname { get; set; }
-        public override string ToString()
-        {
-            if (String.IsNullOrEmpty(Lastname))
-                return base.ToString();
-            return Lastname;
-        }
         public abstract int Course { get; set; }
         public abstract int CreditBookNumber { get; set; }
         public abstract void Print();
@@ -153,10 +138,11 @@ namespace Day_11_Задание_1
 
         public override int Course { get; set; }
         public override int CreditBookNumber { get; set; }
-        public Student() { }
+        public Student(string lastname, int course, int creditbooknumber) 
+        { Lastname = lastname; Course = course; CreditBookNumber = creditbooknumber; }
        
         public override void Print()
-        { Console.WriteLine($"Студент {Lastname}учится на {Course}-ом(ем) курсе. Номер зачетной книги: {CreditBookNumber}"); }
+        { Console.WriteLine($"Студент {Lastname} учится на {Course}-ом(ем) курсе. Номер зачетной книги: {CreditBookNumber}"); }
 
     }
     public class Aspirant : Educated
@@ -166,8 +152,10 @@ namespace Day_11_Задание_1
         public override int Course { get; set; }
         public override int CreditBookNumber { get; set; }
         public int Dissertation { get; set; }
-        public Aspirant() { }
-        
+        public Aspirant(string lastname, int course, int creditbooknumber, int dissertation)
+        { Lastname = lastname; Course = course; CreditBookNumber = creditbooknumber; Dissertation = dissertation; }
+
+
         public override void Print()
         { Console.WriteLine($"Студент {Lastname} учится на {Course}-ом(ем) курсе. Номер зачетной книги: {CreditBookNumber}. Номер диссертации: {Dissertation}"); }
 
